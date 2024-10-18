8505c7fe3d92b0fe1b4e3b59b04af0e78041d711
Qudrat <qudratshaik786@gmail.com>
2024-10-18 22:36:38 +0530

    test added for multiple numbers --> returns their sum

--- calculator.test.js
+++ calculator.test.js
@@ -15,3 +15,10 @@ test('Two numbers should return their sum', () => {
     expect(Add("1,2")).toBe(3);
     expect(Add("10,20")).toBe(30);
 });
+
+
+test('Multiple numbers should return their sum', () => {
+    expect(Add("1,2,3,4,5")).toBe(15);
+    expect(Add("5,10,15,20")).toBe(50);
+    expect(Add("5.5,10.5,19,10.50")).toBe(45.5);
+});
