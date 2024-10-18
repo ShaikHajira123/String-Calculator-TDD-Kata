1840645bbfdbdac7dc2da62f63b15d5709d6de40
Qudrat <qudratshaik786@gmail.com>
2024-10-18 22:32:17 +0530

    test added 2 numbers --> retuns their sum

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
 function Add(numbers) {
     if (numbers === "") return 0;
 
-    return parseFloat(numbers);
+    const numbersArray = numbers.split(",");
+    return numbersArray.reduce((acc, num) => acc + Number(num), 0);
 }
 
 module.exports = Add;
--- calculator.test.js
+++ calculator.test.js
@@ -9,3 +9,9 @@ test('One number should return the number itself', () => {
     expect(Add("10")).toBe(10);
     expect(Add("1.3,")).toBe(1.3);
 });
+
+
+test('Two numbers should return their sum', () => {
+    expect(Add("1,2")).toBe(3);
+    expect(Add("10,20")).toBe(30);
+});
